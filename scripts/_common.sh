4a9a50f35c26d9e870853a87bc395b6a5fa06921
mhfowler <maxhfowler@gmail.com>
2021-10-06 17:13:38 +0200

    Add expect as a dependency

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies=""
+pkg_dependencies="expect"
 
 #=================================================
 # PERSONAL HELPERS
